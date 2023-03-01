c4d491769ce15b9dd6ed8f92da5ef19710660892
duggurd <alexanderhaugerud@gmail.com>
2023-03-01 01:07:34 -0800

    Now inherits from keras.layers.TextVectorization

--- src/models/tokenizer.py
+++ src/models/tokenizer.py
@@ -3,9 +3,19 @@ from keras.layers import TextVectorization
 
 
 class Tokenizer(TextVectorization):
+    """
+    Simple abstract wrapper class for `keras.layers.TextVectorization`.
 
+    The class inherits from the `TextVectorization` class.
+    """
 
-    def __init__(self, max_tokens=15000, sequence_length=100, vocabulary=None):
+    def __init__(
+            self, 
+            max_tokens:int=15000, 
+            sequence_length:int=100, 
+            vocabulary=None
+        ) -> None:
+        
         """
         Construct the tokenizer. This tokenizer strips punctuation and lowercases text.
 
@@ -16,18 +26,15 @@ class Tokenizer(TextVectorization):
         sequence_length: upper limit,
             the maximum number of words to tokenize in a sequence, the tokenizer outputs `sequence_length` tokens.
         """
-
-        self.max_tokens = max_tokens
-        self.sequence_length = sequence_length
-
-        self.tokenizer = TextVectorization(
-            max_tokens=self.max_tokens,
+        
+        super().__init__(
+            max_tokens=max_tokens,
             standardize="lower_and_strip_punctuation",
             output_mode="int",
-            output_sequence_length=self.sequence_length,
+            output_sequence_length=sequence_length,
             vocabulary=vocabulary
         )
-    
+
 
     def decode(self, tokens_iterable:list) -> list:
         """
@@ -36,7 +43,7 @@ class Tokenizer(TextVectorization):
         Returns an empty string if token not in vocabulary.
         """
 
-        vocabulary = self.tokenizer.get_vocabulary()
+        vocabulary = self.get_vocabulary()
         decoded = []
 
         for tokens in tokens_iterable:
@@ -46,6 +53,7 @@ class Tokenizer(TextVectorization):
                 
                 try:
                     tmp_decoded.append(vocabulary[token])
+                
                 except IndexError:
                     tmp_decoded.append("")
             
@@ -55,7 +63,7 @@ class Tokenizer(TextVectorization):
 
 
     @classmethod
-    def from_vocab_file(cls, filepath:str, sequence_length):
+    def from_vocabulary_file(cls, filepath:str, sequence_length) -> Tokenizer:
         """
         Construct Tokenizer from a vocabulary file.
 
@@ -68,15 +76,16 @@ class Tokenizer(TextVectorization):
         tokenizer.load_vocabulary(filepath)
 
         return tokenizer
+        
 
 
     def save_vocabulary(self, filepath) -> None:
         with open(filepath, "w") as file:
-            file.write("\n".join(self.tokenizer.get_vocabulary()))
+            file.write("\n".join(self.get_vocabulary()))
 
 
     def load_vocabulary(self, filepath) -> list:
         with open(filepath, "r") as file:
             vocabulary = file.read().split("\n")
 
-        self.tokenizer.set_vocabulary(vocabulary)
+        self.set_vocabulary(vocabulary)
