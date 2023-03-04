30b2906722c5a88fdd77776ceae18bfe3619cd1c
duggurd <alexanderhaugerud@gmail.com>
2023-03-04 09:45:21 +0100

    Model trainer class for default setup of training

---
+++ src/models/deep_learning/ModelTrainer.py
@@ -0,0 +1,74 @@
+from keras import Model
+from keras.callbacks import Callback, ModelCheckpoint, EarlyStopping
+from keras.metrics import Metric, Accuracy
+from keras.losses import Loss, MeanSquaredError, MeanAbsoluteError
+from keras.optimizers import Optimizer, Adam
+from typing import Optional, List, Union, Literal
+from os import PathLike
+
+class ModelTrainer:
+    def __init__(
+            self,
+            model: Model,
+            optimizer: Optimizer = Adam(),
+            loss: Loss = MeanSquaredError(),
+            metrics: List[Metric] = MeanAbsoluteError(),
+            callbacks: Union[List[Callback], Literal["default"]] = "default",
+            save_path: Optional[Union[str, PathLike]] = None
+        ) -> None:
+        
+
+        self.is_fit = False
+
+        self.model = model
+        self.optimizer = optimizer
+        self.loss = loss
+        self.metrics = metrics if metrics else Accuracy()
+        
+        self.callbacks = callbacks if callbacks != "default" else [
+            ModelCheckpoint(
+                "../../checkpoints/2d_cnn_32_16_32_250k/model",
+                save_best_only=True,
+                save_weights_only=False
+            ),
+            EarlyStopping(
+                patience=5,
+                restore_best_weights=True
+            )
+        ]
+
+        self.save_path = save_path
+
+    def compile(self, **kwargs) -> None:
+        self.model.compile(
+            optimizer = self.optimizer,
+            loss = self.loss,
+            metrics = self.metrics,
+            **kwargs
+        )
+    
+      
+    def fit(
+            self,
+            x, 
+            y,
+            batch_size,
+            epochs,
+            vaiidation_data: tuple,
+            **kwargs
+            
+        ) -> dict:
+
+        self.history = self.model.fit(
+            x=x,
+            y=y,
+            batch_size=batch_size,
+            epochs=epochs,
+            validation_data=vaiidation_data,
+            callbacks=self.callbacks
+            **kwargs
+        )
+
+        self.is_fit = True
+
+        return self.history
