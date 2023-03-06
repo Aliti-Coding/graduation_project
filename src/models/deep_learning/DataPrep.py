9d9765acecc5146d28ebf1d1b7faf2570c3f3315
duggurd <alexanderhaugerud@gmail.com>
2023-03-06 12:42:10 +0100

    Fixed some minor bugs

--- src/models/deep_learning/DataPrep.py
+++ src/models/deep_learning/DataPrep.py
@@ -1,9 +1,9 @@
 
-from transformers import PreTrainedTokenizer
+from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 from sklearn.model_selection import train_test_split
 from keras.layers import TextVectorization
 
-from typing import Optional, Union
+from typing import Optional, Union, Tuple, Iterable, Any
 
 
 class DataPrep:
@@ -14,36 +14,48 @@ class DataPrep:
             y,
             tokenizer: Union[PreTrainedTokenizer, TextVectorization],
             test_size: Optional[float] = 0.2,
-            random_state: Optional[int] = None
+
+            random_state: Optional[int] = None,
+            max_length: Optional[int] = None
         )-> None:
 
         self.x = x
         self.y = y
         self.tokenizer = tokenizer
         self.random_state = random_state
+        self.test_size = test_size
+        self.max_length = max_length
 
-    def data(self):
-        return train_test_split(
-            self._tokenize(self.x),
+    def data(self) -> Tuple[Iterable[Any], Iterable[Any], Iterable[Any], Iterable[Any]]:
+        x_train, x_test, y_train, y_test = train_test_split(
+            self.x,
             self.y,
-            test_size = 0.2,
+            test_size = self.test_size,
             random_state = self.random_state
-        )     
+        )
+
+        x_train = self._tokenize(x_train)
+        x_test = self._tokenize(x_test)
+
+        return x_train, x_test, y_train, y_test
 
     
     def _tokenize(self, x):
         if isinstance(self.tokenizer, TextVectorization):
             return self.tokenizer.call(x)
         
-        elif isinstance(self.tokenizer, PreTrainedTokenizer):
-            return self.tokenizer.tokenize(
+        elif (
+            isinstance(self.tokenizer, PreTrainedTokenizer)
+            or
+            isinstance(self.tokenizer, PreTrainedTokenizerFast)
+        ):
+            return dict(self.tokenizer(
                 x,
                 padding=True,
                 truncation=True,
-                return_tensors="tf"
-            )
-        
+                return_tensors="tf",
+                max_length=self.max_length
+            ))
         else:
-            raise NotImplementedError(
-                f"Not implemented for tokenizer of type: {type(self.tokenizer)}"
-            )
+            raise NotImplementedError(f"Not implemented for tokenizer of type: {type(self.tokenizer)}")
+           
