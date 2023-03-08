b461021ee602a0b84997ba5cee7cafec44b75257
duggurd <alexanderhaugerud@gmail.com>
2023-03-08 09:48:42 +0100

    Still WIP, updating methods

--- src/etl/amazon_reviews/ArrowAmazonReviewsExtractor.py
+++ src/etl/amazon_reviews/ArrowAmazonReviewsExtractor.py
@@ -41,62 +41,14 @@ class ArrowAmazonReviewsExtractor:
         """
         If possible convert_dates should never be called on the column `"reviewTime"` as pandas struggles to parse it, use `"unixReviewTime"` instead.    
         
-        Time complexity to load and transform grows more than proportionally with chunksize ie. two chunks of 500_000 rows loads faster than one at 1_000_000 rows.
+
+        Does not support chunking, meaning entire dataset must fit in memory. However, very effective on datasets up tp 3 million rows, about 300 MBs.
+        WARNING: Work in progress.
         
         ## Params
-        path_or_buf: pathlike,
-            path to file to load data from, should be `.json`,
-            can be compressed as long as pandas is able to infer compression type.
-
-        chunksize: int,
-            the size of each chunk to load from data source (`path_or_buf`).
-
-        features: list of feature names, optional,
-            the features to extract from each chunk/DataFrame, 
-            might have downstream effects on pipeline depending on what is included.
-
-        maximum_words: int, optional,
-            if specified sets the maximum number of words a reviewText can have, 
-            longer are cut to length of maximum_words.
-        
-        review_text_column: str,
-            specifies which column contains the reviews text. Default is "reviewText".
-
-        drop_empty_reviews: bool,
-            drop rows where reviews only contain empty strings.
-
-        ratings_column: str,
-            specifies the name of the column that contains the ratings of the reviews. 
-            Default is "overall".
-        
-        balance_num_pos_neg_ratings: bool,
-            undersampling so that the total number of positive (`rating>3`) and negative (`rating<3`) reviews are equal.
-            The ratings column and review text column must be in the DataFrame.
-
-        balance_neutral_reviews: bool,
-            balance the number of neutral reviews (`rating==3`) to the average of negative and positive reviews.
-
-        convert_dates: bool or list[str],
-            optimistic if using bool, or specify the names of columns to convert to datetime objects.
-
-        outdir: pathlike, optional,
-            if specified overloads iterator functionality, 
-            now saves each chunk to `outdir` instead of returning a DataFrame.
-        
-        save_method: function or callable, optional,
-            if not specified chunks are saved as `.parquet` files. Use this variable to save chunks in other file formats.
-            The callable should take two arguments a `DataFrane` and a `PathLike` used to overide saving method.
-
-
-        ## Examples
-
-        Example `save_method` function: 
-            >>> lambda df, path: pd.DataFrame.to_json(df, path)
-
-        
-
-        
         """
+
+        
         self.json_read_opts = pa.json.ReadOptions(
             use_threads = True,
             block_size = blocksize if blocksize else 100_000
@@ -197,100 +149,3 @@ class ArrowAmazonReviewsExtractor:
         #         df[column] = pd.to_datetime(df[column])
         
         return df
-
-
-    def _balance_reviews(
-            self, 
-            df: pd.DataFrame
-        ) -> pd.DataFrame:
-
-        """
-        TODO:
-        - Implement option for oversampling.
-        - Alpha property which determines the ratio of undersampling to oversampling.
-
-        Balances the number of negative and positive reviews in a chunk 
-        if `balance_num_neg_pos_reviews` is `True` by undersampling.
-
-        Balances the number of neutral reviews if `balance_neutral` is `True`, 
-        by undersampling.
-        """
-
-        if not (
-            self.ratings_column in df.columns \
-            and \
-            self.review_text_column in df.columns
-        ):
-            raise ValueError("Ratings column and review text column must be in DataFrame to balance reviews.")
-
-        value_counts = df[self.ratings_column].value_counts()
-        num_positive = value_counts[4] + value_counts[5]
-        num_negative = value_counts[1] + value_counts[2]
-
-        if num_positive > num_negative:
-            rows_to_drop = num_positive - num_negative
-
-            index = self._rs.choice(
-                a=df.query("overall == 4 or overall == 5").index, 
-                size=rows_to_drop, 
-                replace=False
-            )
-        
-        elif num_negative > num_positive:
-            rows_to_drop = num_negative - num_positive
-            
-            index = self._rs.choice(
-                a=df.query("overall == 1 or overall == 2").index, 
-                size=rows_to_drop, 
-                replace=False
-        )
-
-        df_balanced = df.drop(
-            axis=1,
-            index=index
-        )
-
-        if self.balance_neutral_reviews:
-            balanced_value_counts = df_balanced[self.ratings_column].value_counts()
-
-            num_neutral = balanced_value_counts[3]
-            num_non_neutral = df_balanced[self.ratings_column].count() - num_neutral
-
-            avg_value_count_non_neutral = num_non_neutral / 4
-
-            if num_neutral > avg_value_count_non_neutral:
-                rows_to_drop = num_neutral - avg_value_count_non_neutral
-
-                index = self._rs.choice(
-                    a=df.query("overall == 3").index, 
-                    size=rows_to_drop, 
-                    replace=False
-                )
-
-                df_balanced = df.drop(
-                    axis=1,
-                    index=index
-                )
-
-
-        return df_balanced
-        
-    def _save(
-            self, 
-            table: pa.Table
-        ) -> None:
-
-        """
-        Saves the chunk to `self.outdir` if it has been specified.
-
-        Alternatively uses the callable stored in `self.save_method` to save the chunk.
-        """
-        old_filename = self.path_or_buf.parts[-1].split(".")[0]
-        save_path = f"{self.outdir}/{old_filename}_{self._loaded_chunks}"
-
-        if self.save_method:
-            self.save_method(df, save_path)
-        
-        else:
-            save_path += ".parquet"
-            df.to_parquet(save_path, index=False)
